f1483ce6da4f336ce50aeb9d680a2ae059bf7ee9
Daniel Stephanio Nunes <dti584@localiza.com>
2023-03-14 15:06:46 -0300

    a

--- Program.cs
+++ Program.cs
@@ -2,25 +2,31 @@
 linha), armazenando-os em uma lista. Depois, ordenar os dados dessa lista e mostra-los
 ordenadamente na tela. Nota: o caminho do arquivo pode ser informado "hardcode".*/
 
-string patch = @"C:\Users\198802\Desktop\COISAS\Learn\Learn\Exercicio_POO_21_IComparable\list.txt";
-try
+public class Program
 {
-    using (StreamReader stream = File.OpenText(patch))
+    public static void Main(string[] args)
     {
-        List<string> list = new List<string>();
-        while (!stream.EndOfStream)
+        string patch = @"C:\Users\198802\Desktop\COISAS\Learn\Learn\Exercicio_POO_21_IComparable\list.txt";
+        try
         {
-            list.Add(stream.ReadLine());
+            using (StreamReader stream = File.OpenText(patch))
+            {
+                List<string> list = new List<string>();
+                while (!stream.EndOfStream)
+                {
+                    list.Add(stream.ReadLine());
+                }
+                list.Sort();
+                foreach (string line in list)
+                {
+                    Console.WriteLine(line);
+                }
+            }
         }
-        list.Sort();
-        foreach (string line in list)
+        catch (IOException e)
         {
-            Console.WriteLine(line);
+            Console.WriteLine("Error");
+            Console.WriteLine(e.Message);
         }
     }
-}
-catch (IOException e)
-{
-    Console.WriteLine("Error");
-    Console.WriteLine(e.Message);
 }
