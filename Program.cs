7a82dd5f47b702c8a53df07c1810b66b9cb3c49f
Daniel Stephanio Nunes <dti584@localiza.com>
2023-03-14 15:05:57 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/*Faça um programa para ler um arquivo contendo nomes de pessoas (um nome por
+linha), armazenando-os em uma lista. Depois, ordenar os dados dessa lista e mostra-los
+ordenadamente na tela. Nota: o caminho do arquivo pode ser informado "hardcode".*/
+
+string patch = @"C:\Users\198802\Desktop\COISAS\Learn\Learn\Exercicio_POO_21_IComparable\list.txt";
+try
+{
+    using (StreamReader stream = File.OpenText(patch))
+    {
+        List<string> list = new List<string>();
+        while (!stream.EndOfStream)
+        {
+            list.Add(stream.ReadLine());
+        }
+        list.Sort();
+        foreach (string line in list)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("Error");
+    Console.WriteLine(e.Message);
+}
